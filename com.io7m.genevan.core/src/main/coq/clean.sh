048dce1d50b8e8fc54025a46261def37a73b4fef
Mark Raynsford <code@io7m.com>
2022-08-20 14:14:30 +0000

    Update "clean" script

--- com.io7m.genevan.core/src/main/coq/clean.sh
+++ com.io7m.genevan.core/src/main/coq/clean.sh
@@ -1,5 +1,10 @@
 #!/bin/sh -ex
 
-rm -f Genevan/*.glob Genevan/*.vo Genevan/*.vok Genevan/*.vos Genevan/.*.aux
+find Genevan -name '*.glob' -type f -exec rm -v {} \;
+find Genevan -name '*.vo'   -type f -exec rm -v {} \;
+find Genevan -name '*.vok'  -type f -exec rm -v {} \;
+find Genevan -name '*.vos'  -type f -exec rm -v {} \;
+find Genevan -name '*.aux'  -type f -exec rm -v {} \;
+
 rm -rf html
 
